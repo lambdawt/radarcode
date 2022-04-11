4e857419899436eeb80334a34466cc82d0b19ff2
wtzhu <1240355745@qq.com>
2022-04-11 08:56:58 +0800

    add 雷达系统设计MATLAB仿真 book's pdf and code

---
+++ 雷达系统设计MATLAB仿真_pdf_code/MATLAB Code/chap2/initialize.m
@@ -0,0 +1,8 @@
+clear all
+swid = 1;
+pfa = 1e-7;
+np = 1;
+R_1st_frame = 106e3; % Range for first frame
+R0 = 90e3; % range to last frame 
+SNR0 = 8.5; % SNR at R0
+frame = 0.8e3; % frame size
---
+++ 雷达系统设计MATLAB仿真_pdf_code/MATLAB Code/chap6/initialize.m
@@ -0,0 +1,16 @@
+clear all
+sigma0 = -20;
+thetaA = 1;
+thetaE = 2;
+SL = -20;
+hr = 3;
+ht = 100;
+pt = 75;
+f0 = 5.6e9;
+b = 1e6;
+t0 = 290;
+f = 6;
+l = 10;
+ant_id = 1;
+Rmin = 2;
+Rmax = 50;
